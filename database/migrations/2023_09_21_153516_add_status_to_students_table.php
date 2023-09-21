3fb200049350e53945d371c6a64eab33de49a98f
nehaPandit96 <npandit165@gmail.com>
2023-09-21 11:50:53 -0400

    Student's status migration file added

---
+++ database/migrations/2023_09_21_153516_add_status_to_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('student', function (Blueprint $table) {
+            $table->enum('status', ['NEW', 'ONGOING', 'COMPLETED', 'PENDING REVIEW', 'ARCHIVED', 'SHORTLISTED', 'EXPIRED'])->default('NEW');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('student', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
